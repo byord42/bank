65e713d0a91d6589bd35ecfd57b34ee842782245
VitorDominguesR <vitor_dmrb@hotmail.com>
2019-08-03 14:18:24 -0300

    Create BankAccount.hs

---
+++ BankAccount.hs
@@ -0,0 +1,62 @@
+module Main where
+import BankAccount( closeAccount
+    , incrementBalance
+    , decrementBalance
+    , getBalance
+    , openAccount
+    )
+import Lib
+import Data.Typeable
+import Control.Monad
+
+
+main :: IO ()
+
+main = do  
+    putStrLn "Hello, what's your name?"  
+    userInput <- getLine
+    putStrLn ("Hello " ++ userInput ++ ", welcome to Functional Bank LTDA")
+    account <- openAccount
+    putStrLn ("Now that we created your account you can do those actions")
+    -- get input from user
+
+    -- loop using recursion
+    let loop = do
+        -- echo back to the user
+        putStrLn("1 - Deposit")
+        putStrLn("2 - Withdraw")
+        putStrLn("q - Quit!")
+        userInput <- getLine
+        case userInput of
+            "1" -> do
+                    putStrLn("How much ?")
+                    value <- getLine
+                    let depositValue = (read :: String -> Integer) value
+                    balance <- incrementBalance account depositValue
+                    putStrLn("Your deposit has been made")
+            "2" -> do
+                    putStrLn("How much ?")
+                    value <- getLine
+                    let witdrawValue = (read :: String -> Integer) value
+                    balance <- decrementBalance account witdrawValue
+                    putStrLn("Your witdraw has been made")
+            "q" -> putStrLn("Good Bye !")
+            
+        -- putStrLn ("Hey " ++ userInput ++ ", you rock!")
+        -- putStrLn("How much do you have?")
+        
+        --account <- openAccount
+        --- putStrLn("How much do you have?")
+        balance <- getBalance account
+        -- putStrLn ("type of action2 is: " ++ (show (typeOf balance)))
+        print balance
+        --balance <- incrementBalance account 10
+        --Just balance -> putStrLn $ "Value: " ++ show balance
+        case balance of
+            Just a -> putStrLn $ "Your balance is :"++show a++"."
+        -- quando o userInput for q ele sai do loop
+        when (userInput /= "q") loop
+    loop  -- start the first iteration 
+    putStrLn "Good bye!"
+
+
