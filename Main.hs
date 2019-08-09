fd4a5c0c82897e5b9dd3f3224184e06b0cf651d9
byord42 <52893090+byord42@users.noreply.github.com>
2019-08-09 13:31:58 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -18,17 +18,18 @@ module Main where
         userInput <- getLine
         putStrLn ("Olá " ++ userInput ++ " , bem vindo ao Banco Carlo Kleber LTDA")
         account <- openAccount
-        putStrLn ("Agora que sua conta foi criada, veja as ações que você pode tomar: ")
+        putStrLn ("Agora sua conta foi criada! Você tem um total de 0 reais.")
     
         -- loop using recursion
         --  but do notation is merely syntactic sugar for nested applications of (>>=), which is itself nothing more than an infix higher-order function
         -- http://www.haskellforall.com/2013/07/statements-vs-expressions.html
         let loop = do
             -- echo back to the user
+            putStrLn("Que ação você deseja realizar?")
             putStrLn("1 - Depositar")
             putStrLn("2 - Sacar")
             putStrLn("3 - Projetar o rendimento do seu dinheiro")
-            putStrLn("q - Sair")
+            putStrLn("4 - Sair")
             userInput <- getLine
             case userInput of
                 "1" -> do
@@ -53,28 +54,27 @@ module Main where
                         putStrln("Seu saldo atual é de " ++ balance ++ " reais. Você gostaria de fazer a projeção de rendimento para quantos dias?")
                         let readDays <- getLine
                         let days = (read :: String -> Integer) readDays
-                        let rendimentoDiario = (\x -> x) 0,02 --lambda termo
+                        let rendimento = (\x -> 1 + x) 0,02 --lambda termo
                         rendimentoFinal :: Integer -> Integer
-                        let rendimentoFinal = (balance*((1 + rendimentoDiario)^days))
+                        let rendimentoFinal = (balance*(rendimento^days))
                         putStrln("Daqui a " ++ days ++ " dias o seu saldo será de " ++ rendimentoFinal ++ " reais se você não realizar nenhuma movimentação na conta.")
-                "q" -> putStrLn("Até mais!")
+                "4" -> putStrLn("Até mais!")
                 
             -- putStrLn ("Hey " ++ userInput ++ ", you rock!")
             -- putStrLn("How much do you have?")
             
             --account <- openAccount
             --- putStrLn("How much do you have?")
-            balance <- getBalance account
+            ---balance <- getBalance account
             -- putStrLn ("type of action2 is: " ++ (show (typeOf balance)))
-            print balance
+            ---print balance
             --balance <- incrementBalance account 10
             --Just balance -> putStrLn $ "Value: " ++ show balance
-            case balance of
-                Just a -> putStrLn $ "Your balance is :"++show a++"."
+            --case balance of
+                --Just a -> putStrLn $ "Your balance is :"++show a++"."
             -- quando o userInput for q ele sai do loop
-            when (userInput /= "q") loop
-        loop  -- start the first iteration 
-        putStrLn "Good bye!"
+            when (userInput /= "4") loop
+        --loop  -- start the first iteration
     
     
     
